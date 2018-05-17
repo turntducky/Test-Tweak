d64a6900c8ae2c12237a6bcc0d840771f31ddaeb
Turnt Ducky <ecdenham@gmail.com>
2018-05-17 00:40:21 -0500

    Update DTTRootListController.h

--- testtweakprefs/DTTRootListController.h
+++ testtweakprefs/DTTRootListController.h
@@ -3,13 +3,3 @@
 @interface DTTRootListController : PSListController
 
 @end
-
-@interface FBSystemService : NSObject
-+(id)sharedInstance;
-- (void)exitAndRelaunch:(bool)arg1;
-@end
-
-static void RespringDevice()
-{
-  [[%c(FBSystemService) sharedInstance] exitAndRelaunch:YES];
-}
